79208ba15432ce08300970468228888116665984
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2022-03-13 02:03:35 +0700

    Initial commit

---
+++ SwiftUI T11/SwiftUI_T11App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_T11App.swift
+//  SwiftUI T11
+//
+//  Created by M Fadli Zein on 13/03/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_T11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
